d83ad23689597c4b5a9d711b8bc4b780cb43500f
Trevor Arjeski <tmarjeski@gmail.com>
2024-04-24 21:06:51 +0300

    add atom support

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # rss2json
 
-Minimal "glue" cli tool to read in a RSS (XML) feed and output it to json.
+Minimal *nix filter to read in a RSS/Atom (XML) feed and output it to JSON.
+
+## Usage
+
+```sh
+# Read from stdin:
+cat feed.xml | rss2json
+# Read from file:
+rss2json feed.xml
+```
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::fs::File;
-use std::io::{stdin, stdout, BufReader, IsTerminal, Read};
+use std::io::{stdin, stdout, IsTerminal, Read};
 use std::path::PathBuf;
 
 use anyhow::{bail, Result};
 use argh::FromArgs;
+use atom_syndication::Feed;
 use rss::Channel;
 
 /// A tool to parse and output RSS feeds
@@ -15,14 +16,19 @@ pub(crate) struct Args {
 }
 
 fn main() -> Result<()> {
-    let input: Box<dyn Read> = if let Some(path) = &argh::from_env::<Args>().input_file {
-        Box::new(File::open(path)?)
+    let mut input = String::new();
+    if let Some(path) = &argh::from_env::<Args>().input_file {
+        let mut f = File::open(path)?;
+        f.read_to_string(&mut input)?;
     } else if !stdin().is_terminal() {
-        Box::new(stdin())
+        stdin().read_to_string(&mut input)?;
     } else {
         bail!("No input")
-    };
+    }
 
-    serde_json::to_writer_pretty(stdout(), &Channel::read_from(BufReader::new(input))?)?;
+    match Channel::read_from(input.as_bytes()) {
+        Ok(rss) => serde_json::to_writer_pretty(stdout(), &rss)?,
+        Err(_) => serde_json::to_writer_pretty(stdout(), &Feed::read_from(input.as_bytes())?)?,
+    }
     Ok(())
 }
